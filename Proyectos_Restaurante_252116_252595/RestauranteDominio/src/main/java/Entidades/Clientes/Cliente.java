b38a5e334f2c8a978bd4cb0277d894f3b9bcf52f
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2025-04-01 19:36:44 -0700

    Conexion BD y creacion de los paquetes para dividir modulos en dominio

---
+++ Proyectos_Restaurante_252116_252595/RestauranteDominio/src/main/java/Entidades/Clientes/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades.Clientes;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author PC Gamer
+ */
+
+public class Cliente implements Serializable {
+    
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
