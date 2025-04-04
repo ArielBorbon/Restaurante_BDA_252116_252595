353b3afe81bafd58465ac904073b6e1c72b3496d
Albertojg05 <alberto.jimenez252595@potros.itson.edu.mx>
2025-04-04 00:51:43 -0700

    Test Registar funcionan

--- Proyectos_Restaurante_252116_252595/RestaurantePersistencia/src/test/java/DAO/Clientes/ClientesDAOTest.java
+++ Proyectos_Restaurante_252116_252595/RestaurantePersistencia/src/test/java/DAO/Clientes/ClientesDAOTest.java
@@ -1,83 +1,83 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
-// */
-//package DAO.Clientes;
-//
-//import DTOS.Clientes.NuevoClienteDTO;
-//import Entidades.Clientes.Cliente;
-//import java.util.Calendar;
-//import java.util.GregorianCalendar;
-//import java.util.List;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// *
-// * @author Laboratorios
-// */
-//public class ClientesDAOTest {
-//    
-//    public ClientesDAOTest() {
-//    }
-//    
-//    @BeforeAll
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterAll
-//    public static void tearDownClass() {
-//    }
-//    
-//    @BeforeEach
-//    public void setUp() {
-//    }
-//    
-//    @AfterEach
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of registrarCliente method, of class ClientesDAO.
-//     */
-//    @Test
-//    public void testRegistrarClienteSinCorreo() {
-//        ClientesDAO clientesDAO = new ClientesDAO();
-//        Calendar fechaRegistro = new GregorianCalendar(2025, Calendar.APRIL, 3);
-//        NuevoClienteDTO nuevoClienteDTO = new NuevoClienteDTO(
-//                "Juanito", 
-//                644552492 ,
-//                fechaRegistro
-//        );
-//        Cliente cliente = clientesDAO.registrarCliente(nuevoClienteDTO);
-//        assertNotNull(cliente.getId());
-//        assertEquals(nuevoClienteDTO.getNombre(),cliente.getNombre());
-//        assertEquals(nuevoClienteDTO.getNumTelefono(), cliente.getNumTelefono());
-//        assertEquals(nuevoClienteDTO.getFechaRegistro(), cliente.getFechaRegistro());
-//    }
-//    
-//    @Test
-//    public void testRegistrarClienteConCorreo() {
-//        ClientesDAO clientesDAO = new ClientesDAO();
-//        Calendar fechaRegistro = new GregorianCalendar(2025, Calendar.APRIL, 3);
-//        NuevoClienteDTO nuevoClienteDTO = new NuevoClienteDTO(
-//                "Pepe",
-//                "pepelocochon123@hotmail.com",
-//                644498212 ,
-//                fechaRegistro
-//        );
-//        Cliente cliente = clientesDAO.registrarCliente(nuevoClienteDTO);
-//        assertNotNull(cliente.getId());
-//        assertEquals(nuevoClienteDTO.getNombre(),cliente.getNombre());
-//        assertEquals(nuevoClienteDTO.getCorreo(),cliente.getCorreo());
-//        assertEquals(nuevoClienteDTO.getNumTelefono(), cliente.getNumTelefono());
-//        assertEquals(nuevoClienteDTO.getFechaRegistro(), cliente.getFechaRegistro());
-//    }
-//
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package DAO.Clientes;
+
+import DTOS.Clientes.NuevoClienteDTO;
+import Entidades.Clientes.Cliente;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class ClientesDAOTest {
+    
+    public ClientesDAOTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of registrarCliente method, of class ClientesDAO.
+     */
+    @Test
+    public void testRegistrarClienteSinCorreo() {
+        ClientesDAO clientesDAO = new ClientesDAO();
+        Calendar fechaRegistro = new GregorianCalendar(2025, Calendar.APRIL, 3);
+        NuevoClienteDTO nuevoClienteDTO = new NuevoClienteDTO(
+                "Juanito", 
+                644552492 ,
+                fechaRegistro
+        );
+        Cliente cliente = clientesDAO.registrarCliente(nuevoClienteDTO);
+        assertNotNull(cliente.getId());
+        assertEquals(nuevoClienteDTO.getNombre(),cliente.getNombre());
+        assertEquals(nuevoClienteDTO.getNumTelefono(), cliente.getNumTelefono());
+        assertEquals(nuevoClienteDTO.getFechaRegistro(), cliente.getFechaRegistro());
+    }
+    
+    @Test
+    public void testRegistrarClienteConCorreo() {
+        ClientesDAO clientesDAO = new ClientesDAO();
+        Calendar fechaRegistro = new GregorianCalendar(2025, Calendar.APRIL, 3);
+        NuevoClienteDTO nuevoClienteDTO = new NuevoClienteDTO(
+                "Pepe",
+                "pepelocochon123@hotmail.com",
+                644498212 ,
+                fechaRegistro
+        );
+        Cliente cliente = clientesDAO.registrarCliente(nuevoClienteDTO);
+        assertNotNull(cliente.getId());
+        assertEquals(nuevoClienteDTO.getNombre(),cliente.getNombre());
+        assertEquals(nuevoClienteDTO.getCorreo(),cliente.getCorreo());
+        assertEquals(nuevoClienteDTO.getNumTelefono(), cliente.getNumTelefono());
+        assertEquals(nuevoClienteDTO.getFechaRegistro(), cliente.getFechaRegistro());
+    }
+
 //    /**
 //     * Test of filtrarPorNombre method, of class ClientesDAO.
 //     */
@@ -116,12 +116,12 @@
 //        assertNotNull(clientesConsultados);
 //        assertEquals(NUMERO_DE_CLIENTES_ESPERADOS, clientesConsultados.size());
 //    }
-//
-//    /**
-//     * Test of mostrarListaClientes method, of class ClientesDAO.
-//     */
-//    @Test
-//    public void testMostrarListaClientes() {
-//    }
-//    
-//}
+
+    /**
+     * Test of mostrarListaClientes method, of class ClientesDAO.
+     */
+    @Test
+    public void testMostrarListaClientes() {
+    }
+    
+}
