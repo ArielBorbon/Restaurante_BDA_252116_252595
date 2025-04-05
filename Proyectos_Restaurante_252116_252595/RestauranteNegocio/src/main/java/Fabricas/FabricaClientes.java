8a1b2de9b5ad47372798644da71bf0b700d14f76
Albertojg05 <alberto.jimenez252595@potros.itson.edu.mx>
2025-04-05 12:57:44 -0700

    FabricaClientes

---
+++ Proyectos_Restaurante_252116_252595/RestauranteNegocio/src/main/java/Fabricas/FabricaClientes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Fabricas;
+
+import BO.ClienteBO.ClienteBO;
+import DAO.Clientes.ClientesDAO;
+import NegocioException.NegocioException;
+
+/**
+ *
+ * @author Alberto Jimenez
+ */
+public class FabricaClientes {
+    public static ClienteBO crearClienteBO() throws NegocioException{
+        ClientesDAO clientesDAO = new ClientesDAO();
+        ClienteBO clienteBO = new ClienteBO(clientesDAO);
+        return clienteBO;
+    }
+}
